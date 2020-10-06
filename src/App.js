9c24b0e076d222908667000724932c414701178d
Muhammad Fakhrulghazi bin Mohd Fouad <muhammadfakhrulghazibinmohdfouad@Muhammads-MBP.realtek>
2020-10-06 21:58:45 +0800

    removed host 3000 mistake

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ onButtonSubmit = () => {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('https://vast-scrubland-15564.herokuapp.com:3000/image', {
+        fetch('https://vast-scrubland-15564.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
